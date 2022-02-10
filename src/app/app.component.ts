f7b59690a4e5ce64475c9b3fe2614d529ce96a03
=Francisco Gonzalez <jefragoga_89@yahoo.com>
2022-02-09 22:48:16 -0600

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,46 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pipes';
+
+  nombreMrv:string = "dc";
+  desarrolladopor:string ="jeSuS FRancisCo gOnzAlEz gAonA";
+  nombreJuice:string = "manZana";
+  personajes: string[]=['Flash','Wonder Woman','Shazam','Cybor','Martian Manhunter'];
+  PI:number = Math.PI;
+  porcentaje:number = 0.44555;
+  arreglo:number[]= [0,1,2,3,4,5,6,7,8,9,10]
+  salarioMensualEur:number=6532.56;
+  salarioMensualMXN:number=45232.56;
+  salarioMensualUSN:number=4299.99;
+  idioma:string='es-MX'; 
+  videoUrl:string='https://www.youtube.com/embed/ID6__O04DIw';
+  fecha1:Date=new Date();
+
+  
+  valorPromesa=new Promise<string>((resolve=>{
+       setTimeout(()=>{
+          resolve('Llego la data');
+       },8500)
+  }))
+
+  
+
+  heroe={
+    nombre:"Bruce Wayne",
+    clave:"Batman",
+    edad:35,
+    direccion:{
+    calle:"Gerald Hill",
+    numero:775,
+    distrito:"Ciudad Gothica",
+    casa:"Mansion Wayne"
+    }
+   }
+
+
+   changeLenguaje(idiom: string): void {
+    this.idioma = idiom;
+ }
+
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
+import { NgModule,LOCALE_ID } from '@angular/core';
+import {registerLocaleData} from '@angular/common';
+import localeMx from '@angular/common/locales/es-MX';
+import localeFr from '@angular/common/locales/fr';
 import { AppComponent } from './app.component';
+import { CapitalizePipe } from './pipes/capitalize.pipe';
+
+registerLocaleData(localeMx,'es-MX');
+registerLocaleData(localeFr);
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CapitalizePipe
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [{
+    provide:LOCALE_ID,
+    useValue:'es-MX'
+  }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
